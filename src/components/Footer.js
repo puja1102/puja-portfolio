353ed13ddb6d84624bc363d994c708aa92bf5c68
puja1102 <pgkakani@myseneca.ca>
2024-06-26 16:00:15 -0600

    updated navbar, footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,17 +11,12 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           <MailchimpForm />
-          <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
-          </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/puja-kakani041102/" target="_blank"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://github.com/puja1102" target="_blank"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://www.instagram.com/puj.aa_11?igsh=aTk1bDExM2Rra3l0" target="_blank"><img src={navIcon3} alt="Icon" /></a>
+              <p>Built by Puja Kakani</p>
             </div>
-            <p>Created by @PUJA KAKANI</p>
-          </Col>
         </Row>
       </Container>
     </footer>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,5 @@
 import { useState, useEffect} from "react";
 import {Navbar, Nav, Container} from "react-bootstrap";
-import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
@@ -32,7 +31,21 @@ export const NavBar = () =>{
     <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">
-            <img src={logo} alt="Logo" />
+        <div class="logo fade-enter-done">
+          <a href="#home" aria-label="home">
+          <div class="logo-container">
+            <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96" width="60" height="60">
+            <title>Logo</title>
+            <g transform="translate(-8.000000, -2.000000)">
+              <g transform="translate(11.000000, 5.000000)">
+                <polygon id="Shape" stroke="#87cefa" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" points="39 0 0 22 0 67 39 90 78 68 78 23"></polygon>
+                <text x="48%" y="60%" fill="#87cefa" font-family="sans-serif" font-size="42px" font-weight="bold" text-anchor="middle">P</text>
+              </g>
+            </g>
+            </svg>
+          </div>
+          </a>
+        </div>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
@@ -45,11 +58,13 @@ export const NavBar = () =>{
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.linkedin.com/in/puja-kakani041102/" target="_blank"><img src={navIcon1} alt="" /></a>
+                <a href="https://github.com/puja1102" target="_blank"><img src={navIcon2} alt="" /></a>
+                <a href="https://www.instagram.com/puj.aa_11?igsh=aTk1bDExM2Rra3l0" target="_blank"><img src={navIcon3} alt="" /></a>
             </div>
-            <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
+            <a href="https://drive.google.com/file/d/17i6I7AgGOnk3wW5fxAdhL2e3A6i3A6Mf/view?usp=drive_link" target="_blank">
+              <button className="vvd"><span>Resume</span></button>
+            </a>
           </span>
         </Navbar.Collapse>
       </Container>
